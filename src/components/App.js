aca1ab6948c4fe2e65dfbe1d34fd732b5ad586cf
Ilia Rebenok <rebenok.ilya13@gmail.com>
2022-12-23 01:02:57 +0700

    added functionality to buttons on the editors as well as the functionality of local data storage for the editors, so after reloading the page all the code wil stay whee it was

--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,43 @@ import { html } from '@codemirror/lang-html';
 import { css } from '@codemirror/lang-css';
 import EditorWrap from './EditorWrap';
 import { EditorView } from '@codemirror/view';
+import useLocalStorage from '../hooks/UseLocalStorage'
+import { useEffect } from 'react';
 
 const extensionsJS = [javascript({ jsx: true }), EditorView.lineWrapping];
-const extensionsXML = [html({ html: true  }), EditorView.lineWrapping];
+const extensionsXML = [html({ html: true }), EditorView.lineWrapping];
 const extensionsCSS = [css({ css: true }), EditorView.lineWrapping];
+
 
 function App() {
 
-  const [html, setHtml] = useState('');
-  const [css, setCss] = useState('');
-  const [js, setJs] = useState('');
+  const [html, setHtml] = useLocalStorage('html', '');
+  const [css, setCss] = useLocalStorage('css', '');
+  const [js, setJs] = useLocalStorage('js', '');
+  const [srcDoc, setSrcDoc] = useState('');
 
-  const srcDoc = `
-  <html>
-    <body>${html}</body>
-    <style>${css}</style>
-    <script>${js}</script>
-  </html>
-  `
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+        </html>
+      `)
+    }, 300)
+
+    return () => clearTimeout(timeOut) 
+    
+  }, [html, css, js])
+
 
   return (
     <div>
       <div className='pane top-pane'>
 
         <EditorWrap language='JS'>
-        <CodeMirror
+          <CodeMirror
             theme={MyTheme}
             onChange={setJs}
             height="100%"
@@ -39,11 +51,11 @@ function App() {
             extensions={extensionsJS}
             value={js}
 
-            />
+          />
         </EditorWrap>
 
         <EditorWrap language='CSS'>
-        <CodeMirror
+          <CodeMirror
             theme={MyTheme}
             onChange={setCss}
             height="100%"
@@ -51,19 +63,19 @@ function App() {
             className='code-mirror-wrapper'
             extensions={extensionsCSS}
             value={css}
-            />
+          />
         </EditorWrap>
 
         <EditorWrap language='HTML'>
-        <CodeMirror
+          <CodeMirror
             theme={MyTheme}
             onChange={setHtml}
             height="100%"
             className='code-mirror-wrapper'
             extensions={extensionsXML}
             value={html}
-            
-            />
+
+          />
         </EditorWrap>
 
       </div>
--- src/components/EditorWrap.js
+++ src/components/EditorWrap.js
@@ -1,11 +1,27 @@
+import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function EditorWrap(props) {
+
+    const [open, setOpen] = useState(true);
+
+    function changeButtonState() {
+        setOpen(prevOpen => ! prevOpen);
+        console.log(`button is pressed, state is ${open}`)
+    }
+    
+
     return (
-        <div className='editor-container'>
+        <div className={`editor-container ${open ? '': 'collapsed'}`}>
             <div className='editor-title'>
-                {props.language}
-                <button>
-                    O/C
+                {props.language}                
+                <button 
+                    className='expand-button' 
+                    onClick={changeButtonState}>
+                    <FontAwesomeIcon icon={open ? faExpandAlt : faCompressAlt}/>
                 </button>
             </div>
             {props.children}
